62cd4273bf50c0ab4140d9b60f5231b01c841c6b
Johann <mast3r1sgr3at@hotmail.com>
2020-08-20 09:44:43 +0800

    initial commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        $url = "http://localhost/GloryFlorist/Controllers/WebServices/queryArrangements.php?minPrice=10";
+        $client = curl_init($url);
+        curl_setopt($client, CURLOPT_RETURNTRANSFER, true);
+        $response = curl_exec($client);
+        $result = json_decode($response);
+        echo var_dump($result);
+        ?>
+    </body>
+</html>
