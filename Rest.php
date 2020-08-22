a17e350502a075290addb44cfffa147ead6ef832
Johann <mast3r1sgr3at@hotmail.com>
2020-08-22 15:53:30 +0800

    REST stuff

--- Rest.php
+++ Rest.php
@@ -23,8 +23,8 @@ class Rest {
 
     public function __construct($url) {
         $client = curl_init($url);
-        curl_setopt($client, CURLOPT_RETURNTRANSFER, true);
-        $this->response = json_decode(curl_exec($client));
+        curl_setopt($client, CURLOPT_RETURNTRANSFER, true); 
+        $this->response = json_decode(curl_exec($client)); 
         $this->status = $this->response->status;
         $this->hasData = $this->response->hasData;
         $this->data = $this->response->data;
